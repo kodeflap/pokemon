2f6390df202bef0b71d9a5f6ecb7319c21db6eed
Pooja shaji <poojashaji321@gmail.com>
2021-12-31 02:07:06 +0530

    Score counter added and screenshot of application

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const ash_char = document.querySelector(".ash_character");
 const ash = document.querySelector(".ash");
 const pikachu = document.querySelector(".pikachu");
-
+var counter = 0;
 function jump()
 {
     if(ash.classList != "animate") {
@@ -19,9 +19,16 @@ document.addEventListener("keydown",function () {
 let isAlive = setInterval(function (){
     let ashTop = parseInt(window.getComputedStyle(ash).getPropertyValue("top"));
     let pikachuleft = parseInt(window.getComputedStyle(pikachu).getPropertyValue("left"));
-    if(pikachuleft < 40 && pikachuleft > 20 && ashTop >= 130)
+    if(pikachuleft < 20 && pikachuleft > -20 && ashTop >= 130)
     {
         pikachu.style.animation = "none";
-        alert("Game Over.Press SpaceBar to restart");
+        alert("\n\nGame Over.Press SpaceBar to restart\n Score: "+Math.floor(counter/100));
+        counter = 0;
+       // pikachu.style.animation = "move 1s infinite linear"
+    }
+    else
+    {
+        counter++;
+        document.getElementById("score").innerHTML = Math.floor(counter/100);
     }
 },10);
